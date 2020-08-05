1fe4af4f171b6ba5d58972dc3cb3ac303d0f42d5
DeveshDongre <devesh.dongre@gmail.com>
2020-08-06 01:53:34 +0530

    added article page

--- blogy_app/urls.py
+++ blogy_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 
     path('users/', views.users, name='user'),
     path('', views.index, name='index'),
+    path('articles/<int:article_id>', views.article, name='article'),
 ]
--- blogy_app/views.py
+++ blogy_app/views.py
@@ -14,3 +14,9 @@ def users(request):
 def index(request):
     articles = Articles.objects.all()
     return render(request, 'blogy_app/index.html', {'articles': articles})
+
+
+def article(request, article_id):
+    article = Articles.objects.filter(id=article_id)
+
+    return render(request, 'blogy_app/article.html', {'art': article and len(article) > 0 and article[0]})
