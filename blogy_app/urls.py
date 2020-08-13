1dbef1139b475cae2c3910a1c0469a5ceaac59ee
DeveshDongre <devesh.dongre@gmail.com>
2020-08-14 03:18:10 +0530

    login and registartion of user

--- blogy_app/urls.py
+++ blogy_app/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
 
     path('users/', views.users, name='user'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_route, name='login_route'),
     path('', views.index, name='index'),
     path('articles/<int:article_id>', views.article, name='article'),
 ]
